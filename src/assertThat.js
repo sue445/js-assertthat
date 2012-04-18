3e41ad9743aa1c42a66f1877ac2232d084435eee
sue445 <sue445@sue445.net>
2012-04-18 12:32:55 +0900

    refactor

--- src/assertThat.js
+++ src/assertThat.js
@@ -10,27 +10,32 @@ var at = (function(){
     var is = {
         equalsTo : function(expected){
             return function(actual){
-                doAssert("[expected " + expected + ", but actual is " + actual + "]", actual == expected);
+                doAssert("[expected " + expected + ", but actual is " + actual + "]",
+                    actual == expected);
             };
         },
         greaterThan : function(expected){
             return function(actual){
-                doAssert("[expected greater than " + expected + ", but actual is " + actual + "]", actual > expected);
+                doAssert("[expected greater than " + expected + ", but actual is " + actual + "]",
+                    actual > expected);
             };
         },
         greaterThanOrEqualTo : function(expected){
             return function(actual){
-                doAssert("[expected greater than or equal to " + expected + ", but actual is " + actual + "]", actual >= expected);
+                doAssert("[expected greater than or equal to " + expected + ", but actual is " + actual + "]",
+                    actual >= expected);
             };
         },
         lessThan : function(expected){
             return function(actual){
-                doAssert("[expected less than " + expected + ", but actual is " + actual + "]", actual < expected);
+                doAssert("[expected less than " + expected + ", but actual is " + actual + "]",
+                    actual < expected);
             };
         },
         lessThanOrEqualTo : function(expected){
             return function(actual){
-                doAssert("[expected less than or equal to " + expected + ", but actual is " + actual + "]", actual <= expected);
+                doAssert("[expected less than or equal to " + expected + ", but actual is " + actual + "]",
+                    actual <= expected);
             };
         }
     };
