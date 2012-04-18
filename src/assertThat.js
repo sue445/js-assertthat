61477ef135bc01ab5e7bf59bf5e0a78b03ea4919
sue445 <sue445@sue445.net>
2012-04-18 17:44:01 +0900

    refactor: initialize like jQuery

--- src/assertThat.js
+++ src/assertThat.js
@@ -1,58 +1,53 @@
-var at = (function(){
-    // public methods
-    var matcher = {
-        equalsTo : function(expected){
-            return function(actual){
-                doAssert("[expected " + expected + ", but actual is " + actual + "]",
-                    actual == expected);
-            };
+(function(window){
+    var at = {
+        matcher: {
+            equalsTo : function(expected){
+                return function(actual){
+                    doAssert("[expected " + expected + ", but actual is " + actual + "]",
+                        actual == expected);
+                };
+            },
+            greaterThan : function(expected){
+                return function(actual){
+                    doAssert("[expected greater than " + expected + ", but actual is " + actual + "]",
+                        actual > expected);
+                };
+            },
+            greaterThanOrEqualTo : function(expected){
+                return function(actual){
+                    doAssert("[expected greater than or equal to " + expected + ", but actual is " + actual + "]",
+                        actual >= expected);
+                };
+            },
+            lessThan : function(expected){
+                return function(actual){
+                    doAssert("[expected less than " + expected + ", but actual is " + actual + "]",
+                        actual < expected);
+                };
+            },
+            lessThanOrEqualTo : function(expected){
+                return function(actual){
+                    doAssert("[expected less than or equal to " + expected + ", but actual is " + actual + "]",
+                        actual <= expected);
+                };
+            }
         },
-        greaterThan : function(expected){
-            return function(actual){
-                doAssert("[expected greater than " + expected + ", but actual is " + actual + "]",
-                    actual > expected);
-            };
+
+        assertThat : function(actual, matcher){
+            matcher(actual);
         },
-        greaterThanOrEqualTo : function(expected){
-            return function(actual){
-                doAssert("[expected greater than or equal to " + expected + ", but actual is " + actual + "]",
-                    actual >= expected);
-            };
+
+        setAssert : function(asserttion){
+            doAssert = asserttion;
         },
-        lessThan : function(expected){
-            return function(actual){
-                doAssert("[expected less than " + expected + ", but actual is " + actual + "]",
-                    actual < expected);
-            };
-        },
-        lessThanOrEqualTo : function(expected){
-            return function(actual){
-                doAssert("[expected less than or equal to " + expected + ", but actual is " + actual + "]",
-                    actual <= expected);
-            };
+
+        doAssert : function(message, isSuccess){
+            // do nothing
         }
-    };
-
-    var assertThat = function(actual, matcher){
-        matcher(actual);
-    };
-
-    var setAssert = function(asserttion){
-        doAssert = asserttion;
     }
 
-    return {
-        matcher : matcher,
-        assertThat : assertThat,
-        setAssert : setAssert
-    };
-
-    // private methods
-    var doAssert = function(message, isSuccess){
-        // do nothing
-    };
-
-}());
+    window.at = at;
+})(window);
 
 is = at.matcher;
 assertThat = at.assertThat;
